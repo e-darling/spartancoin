24c3fd1a439b00dffb581f54de2edeb8daf8868d
Eddie Darling <e.doyle@berkeley.edu>
2021-10-31 15:59:41 -0700

    Add conftest for repr

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+"""
+Configuration for pytest
+"""
+
+
+def pytest_make_parametrize_id(config, val):
+    """
+    Return a user-friendly string representation of the given `val` that will be
+    used by @pytest.mark.parametrize calls, or None if the hook doesn’t know
+    about val.
+    """
+    if getattr(val, "__module__", val.__class__.__module__).startswith("spartancoin"):
+        return repr(val)
+    return None
