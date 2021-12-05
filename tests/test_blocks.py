d7f4f0d8801e89c74bf3351b2f2804e235b204ec
Eddie Darling <e.doyle@berkeley.edu>
2021-12-05 09:38:11 -0800

    Remove non-test

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -4,19 +4,11 @@ Unittest blocks
 
 from __future__ import annotations
 
-import random
-from timeit import timeit
-
 import pytest
 
 from spartancoin.blocks import Block as _Block
 from spartancoin.blocks import BlockSHA256, BlockSHA512
 from spartancoin.transactions import Transaction
-
-
-def rand_block_hash() -> bytes:
-    """Return a dummy `Transaction`"""
-    return b"".join(bytes([b]) for b in random.choices(b"qwertyuiop", k=64))
 
 
 def test_one(tran: Transaction) -> None:
@@ -32,17 +24,3 @@ def test_can_hash(Block: type[_Block], tran: Transaction) -> None:
     h = block.hash()
     assert h[0] == 0
     assert h[1] == 0
-
-
-def test_time(tran: Transaction) -> None:
-    """Test can hash a block"""
-    times = []
-    for i in range(10):
-        # this `block` variable is used in `timeit`
-        print(f"starting {i}")
-        block = BlockSHA512(rand_block_hash(), [tran])  # pylint: disable=W0641
-        t = timeit("block.hash()", number=1, globals=locals())
-        print(f"took {t} seconds")
-        times.append(t)
-    print(f"average of {sum(t) / len(t)} seconds")
-    assert 0
